85d859204ad0517b883b2e94eaa94a98bb9a0828
olasubom <olasubomiadebayo5@gmail.com>
2024-08-13 19:57:59 -0700

    added new codes

--- internship_2.py
+++ internship_2.py
@@ -4,65 +4,65 @@ import random
 import string
 
 
-# def candy_store():
-#     large_jar = ["lollipop" , "chocolate" , "gummies" , "taffies"]
-#     random_candies =[]
-#     random_num  = random.choice([1,2,3,4])
-#     random_candies.append(random_num)
+def candy_store():
+    large_jar = ["lollipop" , "chocolate" , "gummies" , "taffies"]
+    random_candies =[]
+    random_num  = random.choice([1,2,3,4])
+    random_candies.append(random_num)
 
-#     for candies in range(1):
-#         random_candie = random.choice(large_jar)
-#         random_candies.append(random_candie)
+    for candies in range(1):
+        random_candie = random.choice(large_jar)
+        random_candies.append(random_candie)
 
-#     print(random_candies)
-# candy_store()    
-
-
-
-# def savings_plan():
-#     num_of_weeks = int(input("input the  numbers of weeks\n"))
-#     initial_amount = 100
-#     additional_amount = 25
-#     total_savings = initial_amount
-#     current_week = 0
-
-#     while  current_week < num_of_weeks:
-#         total_savings  +=  additional_amount
-#         current_week += 1
-#     print(f"the total savings for {num_of_weeks} weeks is ${total_savings} ")
+    print(random_candies)
+candy_store()    
 
 
-# savings_plan()
 
-# def taxi_fare(distance_kilometer):
+def savings_plan():
+    num_of_weeks = int(input("input the  numbers of weeks\n"))
+    initial_amount = 100
+    additional_amount = 25
+    total_savings = initial_amount
+    current_week = 0
+
+    while  current_week < num_of_weeks:
+        total_savings  +=  additional_amount
+        current_week += 1
+    print(f"the total savings for {num_of_weeks} weeks is ${total_savings} ")
+
+
+savings_plan()
+
+def taxi_fare(distance_kilometer):
     
-#     base_fee = 500
-#     per_kilometer_travelled = 20
-#     total_fee = base_fee + (per_kilometer_travelled * distance_kilometer)
-#     return total_fee
-# kilometer = int(input("the number of kilometer travelled: "))
-# fee = taxi_fare(kilometer)
-# print(f"your total fee for {kilometer} kilometer is ${fee}")
+    base_fee = 500
+    per_kilometer_travelled = 20
+    total_fee = base_fee + (per_kilometer_travelled * distance_kilometer)
+    return total_fee
+kilometer = int(input("the number of kilometer travelled: "))
+fee = taxi_fare(kilometer)
+print(f"your total fee for {kilometer} kilometer is ${fee}")
 
-# print("")
+print("")
 
 
-# def discount_cal(price):
-#     c_f_d = 10000
-#     discount_fee = 0.10
+def discount_cal(price):
+    c_f_d = 10000
+    discount_fee = 0.10
 
-#     if price > c_f_d:
-#         discount = price * discount_fee
-#         final_price = price - discount
+    if price > c_f_d:
+        discount = price * discount_fee
+        final_price = price - discount
 
-#     else:
-#         final_price =  price
+    else:
+        final_price =  price
 
-#     return final_price
-# goods_price = int(input("Enter the price of goods bought: "))
-# final_price = discount_cal(goods_price)
-# print(f"Price: ${goods_price}")
-# print(f"After adding discount: ${final_price} ")
+    return final_price
+goods_price = int(input("Enter the price of goods bought: "))
+final_price = discount_cal(goods_price)
+print(f"Price: ${goods_price}")
+print(f"After adding discount: ${final_price} ")
 
 def lone_calculator():
     lone_amount = int(input("input the amount of  loan collected: "))
@@ -76,6 +76,24 @@ def lone_calculator():
     return calculate_payment()
 total = lone_calculator()
 print(f"the total intrest of the loan is ${total:.2f}")
+
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+       return n * factorial(n - 1)
+
+f_o_c = factorial(int(input("your factorial is ")))
+print(f"your factoria is {f_o_c}") 
+
+def palindrome(word):
+    word = word.lower()
+    return word  == word[::-1]
+word = str(input("input word "))
+result = palindrome(word)
+print(f"is {word} a palindrome? {result}")
+
 
 
 
